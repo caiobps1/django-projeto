4b57213c0136c4235d7bfc78727498429eb3c308
Caio <caiobps1@gmail.com>
2022-07-01 11:52:18 -0300

    criação app pessoas

---
+++ pessoa/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+
+class Pessoa(models.Model):
+    nome_completo = models.CharField(max_length=256)
+    data_nascimento = models.DateField(null=True)
+    ativa = models.BooleanField(default=True)
